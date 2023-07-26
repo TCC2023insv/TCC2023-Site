ac41b6480db65274490abf9edc5b79255cd536f0
Kassa <kassa3b.etec2023@gmail.com>
2023-07-26 18:05:42 -0300

    Co-authored-by: avacuna380@gmail.com <avacuna380@gmail.com>

---
+++ Tema_Claro/p_Diretoria/p_reparos-lab1-D_TC.php
@@ -0,0 +1,55 @@
+<!-- <?php
+    // require("php/conexao/conexaoBD.php");
+
+    // $conexao = ConectarBanco();
+
+    // $sql_query = $conexao->query("SELECT `Data`, `Responsavel`, `Laboratorio` FROM `reparo` WHERE
+    //  Laboratorio = 'Lab 1'") or die ($conexao->error);
+    ?> -->
+
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+        <head>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" type="text/css" href="../../css/p_reparos-labs_TC.css">
+            <link rel="stylesheet" type="text/css" href="../../css/navbar_TC.css">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Reparos</title>
+        </head>
+        <body>
+            <nav>
+                <input type="checkbox" id="check">
+                <label for="check" class="btncheck">
+                    <img src="img/icon.png">
+                </label>
+    
+                <label class="logo">LOGO</label>
+                <ul>
+                    <li><a href="">Cadastros</a></li>
+                    <li><a class="active" href="">Laboratórios</a></li>
+                    <li><a href="../p_login_TC.html">Sair</a></li>
+                </ul>
+            </nav>
+    
+            <h1>Lab 1</h1>
+            <?php
+                $i = 0;
+                while ($i < 10)
+                // while ($reparo = $sql_query->fetch_assoc())
+                {
+            ?>
+    
+            <div id="Bloco">
+                <div class="Itens"><?php echo "<a href='p_rep-registrado_TC.php'>00/00/0000</a>"; ?></div>
+                <div class="Itens"><?php echo "Responsável"; $i++; ?></div>
+            </div>
+            
+            <!-- <?php echo "<td>Data: <a href='p_rep-registrado_TC.php?data=" . $reparo['data']. "'>exemploData</a></td>"; ?>
+            <td><?php echo "Responsável: " . $reparo['Responsavel']; ?></td>
+            <td><?php echo "Laboratório: " . $reparo['Laboratorio']; ?></td> -->
+    
+            <?php
+                } 
+            ?> 
+        </body>
+    </html>
